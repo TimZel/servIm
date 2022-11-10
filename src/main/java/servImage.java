1c120a541021d8a6278b3ab20a4cccff57e28807
TimZel <107506944+TimZel@users.noreply.github.com>
2022-11-10 23:54:28 +0300

    Update servImage.java

--- src/main/java/servImage.java
+++ src/main/java/servImage.java
@@ -1,5 +1,3 @@
-
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.util.*;
@@ -7,76 +5,45 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class servImage {
-
+    public static final HashMap<String, String> mapContentType = new HashMap<>();
+    public static final  String notFound = "HTTP/1.0 404 Not Found\r\n" + "Content-Type: text/html\r\n" + "\r\n";
     public static void main(String[] args) throws IOException {
+        mapContentType.put(".txt", "text/html");
+        mapContentType.put(".html", "text/html");
+        mapContentType.put(".gif", "image/gif");
+        mapContentType.put(".jpeg", "image/jpeg");
+        mapContentType.put(".png", "image/png");
         int visit = 0;
+        String request = "";
+        String filePath = "";
+        String exeType = "";
+        String contentType = "";
         ServerSocket servsock = new ServerSocket(12000);//серверсокет с указанным портом
-            Pattern path = Pattern.compile("(/[^\s]*)");//паттерн пути
-            Pattern exe = Pattern.compile("(\\.[^\s]*)"); //паттерн расширения
-            //открываю потоки
+        Pattern pattern = Pattern.compile("(/[^\s]*)(\\.[^\s]*)");//паттернs
         do {
             try ( var socket = servsock.accept();// сокет соединения с клиентом
                  var isr = new BufferedReader(new InputStreamReader(socket.getInputStream()));//принимаю поток
                  var out = new DataOutputStream(socket.getOutputStream()) ) //вывожу поток
             {
-
-                String request = isr.readLine();//считываю входящую инфу
-                //создаю переменные для обработки информации
-                String filePath = "";
-                String exeType = "";
-                String contentType = "";
-                //создаю объект матчер для поиска возможных совпадений сежду паттернами и поступившей инфой
-                Matcher matcher1 = path.matcher(request);//создаю объект matcher1 типа Matcher на основе поступившей от клиента информации
-                Matcher matcher2 = exe.matcher(request);
-                //создаю мап с парами расширение=контент-тип
-                HashMap<String, String> mapContentType = new HashMap<>();
-                mapContentType.put(".txt", "text/html");
-                mapContentType.put(".html", "text/html");
-                mapContentType.put(".gif", "image/gif");
-                mapContentType.put(".jpeg", "image/jpeg");
-                mapContentType.put(".png", "image/png");
-                //ищу совпадения
-                if (matcher1.find()) {
-                    filePath = matcher1.group();//сохраняю путь
+                request = isr.readLine();//считываю входящую инфу
+                Matcher matcher = pattern.matcher(request);//создаю объект matcher типа Matcher на основе поступившей от клиента информации
+                if (matcher.find()) {
+                    filePath = matcher.group(0);//сохраняю путь
+                    exeType = matcher.group(2);
                 }
-                if (matcher2.find()) {
-                    exeType = matcher2.group();//сохраняю тип расширения
-                }
-                //получаю доступ к парам мапы
-                Set<Map.Entry<String, String>> exeMapContentType = mapContentType.entrySet();
-                for (Map.Entry<String, String> pair : exeMapContentType) {
-                    String key = pair.getKey();//получаю ключ
-                    String value = pair.getValue();//получаю значение
-                    if (key.equals(exeType)) {//если ключ совпадает с расширением
-                        contentType = value;//то контент-типу присваиваем значение
-                    }
-                }
+                contentType = mapContentType.get(exeType);
                 System.out.println("Client " + (++visit) + " accepted.");//информирую об обращении клиента
                 File file = new File(filePath);//создаю файл  и передаю в него полученный путь
                 try (var inF = new FileInputStream(file)) { //открываю поток для чтения файла
-                    String tempBytes = getFileSizeKiloBytes(file);//вызываю статический метод перевода длины файла в объем файла
-                    double tempDBytes = Double.parseDouble(tempBytes);// для конвертации
-                    int qtyBytes;//инициирую переменную
-                    //проверяю дабл-объем
-                    if (tempDBytes < 1.00D) { //округляю (присваиваю новое значение)
-                        qtyBytes = 1024;
-                    } else {
-                        qtyBytes = (int) tempDBytes;//перевожу в инт
-                    }
+                    int qtyBytes = (int) getFileSizeBytes(file);;//инициирую переменную
                     byte[] bytes = new byte[qtyBytes];//создаю байт-массив для хранения информации из файла
-                    String httpResponse =
-                            "HTTP/1.0 200 OK\r\n" +
-                                    "Content-Type: " + contentType + "\r\n" + "\r\n";
+                    String httpResponse = "HTTP/1.0 200 OK\r\n" + "Content-Type: " + contentType + "\r\n" + "\r\n";
                     out.write(httpResponse.getBytes());//отправляю хттп-ответ
                     while ((inF.read(bytes)) > -1) {
                         out.write(bytes, 0, qtyBytes);//отправляю содержимое файла
                     }
                 } catch (FileNotFoundException ex) {
-                    System.out.println("Йок-макарёк, файла-та и нет: " + ex);//шуткую, если файла нет
-                    String notFound =
-                            "HTTP/1.0 404 Not Found\r\n" +
-                                    "Content-Type: text/html\r\n" +
-                                    "\r\n";
+                    System.out.println("Внимание! Розыск: " + ex);//шуткую, если файла нет
                     out.write(notFound.getBytes());
                 }
             }
@@ -84,7 +51,7 @@ public class servImage {
         servsock.close();//закрываю сервер
         System.out.println("Server closed due to settings.");
     }
-    private static String getFileSizeKiloBytes(File file) { //узнаю объем файла
-        return (double) file.length()/1024 + "";
+    private static long getFileSizeBytes(File file) { //узнаю объем файла
+        return file.length();
     }
 }
